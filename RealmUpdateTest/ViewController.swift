af32469389a3aa68507bc833f1f3c606965933f1
Alejandro Menendez <alejandro.menendez.pull@gmail.com>
2017-08-22 11:46:18 +0530

    Initial Commit

---
+++ RealmUpdateTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmUpdateTest
+//
+//  Created by Menlo on 22/08/17.
+//  Copyright © 2017 zooft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
